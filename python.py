e8dd71ebc34da4ff18d56d1b671e612d7b2da6b4
sunbeam <you@example.com>
2023-07-27 14:53:12 +0530

    jhfsj

--- python.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/", methods=["GET"])
-def root():
-    return "welcome to python flask app"
-app.run(host="0.0.0.0", port=80, debug=True)
-
